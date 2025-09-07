5fe9fb79aeb17964a87f80c918cdecad4d7ab754
DuscraperRn <duscraper@gmail.com>
2025-09-07 15:36:04 +0530

    T5

--- src/mycode.py
+++ src/mycode.py
@@ -1,6 +1,6 @@
 def addition(a,b):
     return a+b
 
-def test_add():
-    assert add(1,2)==3
-    assert add(-2,-2)==0
+def test_addition():
+    assert addition(1,2)==3
+    assert addition(-2,-2)==0
